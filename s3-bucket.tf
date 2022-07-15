0fe4c4963c386c977dea97ea30262b2121c6f743
kris <kris@strataconsulting.com>
2022-07-15 03:40:48 +0000

    Added S3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-//  source = "strataconsulting-kris/Registry/Modules/private/s3-bucket/aws"
 
   version = "2.8.0"
+  bucket_prefix = var.prefix  
   acl    = "private"
-  bucket_prefix = var.prefix
 
   versioning = {
     enabled = true
