83b7c4026aca68ed8e610b1a81a8d3ee99469850
kris <kris@strataconsulting.com>
2022-07-18 16:57:08 +0000

    Added s3 bucket module updates

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
-  source = "https://github.com/kris-pandey/terraform-aws-s3-bucket"
-
-  version = "2.8.0"
+  //source = "https://github.com/kris-pandey/terraform-aws-s3-bucket"
+  source = "terraform-aws-modules/s3-bucket/aws"
+  //version = "2.8.0"
   bucket_prefix = var.prefix  
   acl    = "private"
 
